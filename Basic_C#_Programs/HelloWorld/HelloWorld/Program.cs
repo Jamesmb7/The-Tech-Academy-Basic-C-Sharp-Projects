cd5df89d491c5b7eec4177c1621d18ba0a1564cc
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-05 23:16:54 -0500

    Create Program.cs

---
+++ Basic_C#_Programs/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main() //adjectives for this method not important yet
+    {
+        Console.WriteLine("Hello, World!"); /*This will open the console, display hello world, then close it quickly, to prevent the close we must add Console.ReadLine() as shown below.*/
+        Console.ReadLine(); /*This tells the console to stay open until we physically close it*/
+    }
+}
+

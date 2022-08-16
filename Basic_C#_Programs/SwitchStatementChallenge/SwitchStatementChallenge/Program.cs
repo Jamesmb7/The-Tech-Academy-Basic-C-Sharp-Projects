cb57c973ae6162b51b6030a6025fd16da809c027
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-16 09:29:50 -0500

    Do/While Statements and Switch Statement Uploads.

---
+++ Basic_C#_Programs/SwitchStatementChallenge/SwitchStatementChallenge/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchStatementChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 4; //selects the case
+            switch (day) //begins the switch statement and varifies what the day is
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default: //used to set a condition if no other conditions are met aka if I changed the int day to 8, there are only 7 cases/days in a week so it would go to default reply below.
+                    Console.WriteLine("There aren't even this many days in a week!");
+                    break;
+                            
+                    
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/WhileStatementAssignment/WhileStatementAssignment/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileStatementAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess my favorite color!"); //asks a question.
+            string guess = Convert.ToString(Console.ReadLine()); //takes the question and forces it into a string, then applies that answer to the variable "guess".
+            bool isGuessed = guess == "blue"; //states that the variable guess has to = "blue" to be correct, isGuessed will take that information and decide if it's a true or false statement.
+
+            do //do while loop start. Allows you to guess the answser to the question above on your first try and allows the following full switch statement to run. 
+            {
+                switch (guess) //starts a switch statement using case/break.
+                {
+                    case "blue": //sets this case to blue, or the right answer for this question.
+                        Console.WriteLine("You chose the color blue! Good job!"); //console reply telling that you got the answer correct.
+                        isGuessed = true; //tells the program this is the correct answer, or "true" in boolean as stated above guess == "blue". 
+                        break; //ends case "blue" if correct.
+                    default: //Every other answer would be incorrect, default allows that option for a loop so people can continue guessing.
+                        Console.WriteLine("You chose " + guess + "." + " Please guess another color!"); //Informs the user what they chose and tells them to try again.
+                        Console.WriteLine("Guess my favorite color!"); //asks the question again.
+                        guess = Convert.ToString(Console.ReadLine()); //takes the user answer to the question and inserts it as a value for the variable guess.
+                        break; //ends the default case.
+                }
+            }
+            while (!isGuessed); //is saying while the variable isGuessed is not true, do the following.
+            {
+                Console.ReadLine(); //prevents instant closing of the cmd menu.
+            }
+
+            
+
+
+
+                //while (!isGuessed)
+                //{
+                //    switch (guess)
+                //    {
+                //        case "blue":
+                //            Console.WriteLine("You chose the color blue! Good job!");
+                //            isGuessed = true;
+                //            break;
+                //        default:
+                //            Console.WriteLine("You chose " + guess + "." + " Please guess another color!");
+                //            Console.WriteLine("Guess my favorite color!");
+                //            guess = Convert.ToString(Console.ReadLine());
+                //            break;
+                //    }
+                //}
+                //Console.ReadLine();
+        }
+    }
+}

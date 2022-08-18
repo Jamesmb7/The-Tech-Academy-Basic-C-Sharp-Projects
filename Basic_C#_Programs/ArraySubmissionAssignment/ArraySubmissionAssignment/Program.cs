37532abcfecb28799bf8a96456015d65e8a59641
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-18 14:47:24 -0500

    Array Submission Assignment upload

---
+++ Basic_C#_Programs/ArraySubmissionAssignment/ArraySubmissionAssignment/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string[] stringArray = { "James", "Jackie", "Jason", "Jill", "Bill" };
+
+        int[] intArray = { 5, 15, 30, 45, 85 };
+
+        List<string> stringList1 = new List<string>();
+        stringList1.Add("James");
+        stringList1.Add("Jackie");
+        stringList1.Add("Jason");
+        stringList1.Add("Jill");
+        stringList1.Add("Bill");
+
+
+        Console.WriteLine("Please select an index location of my String Array between numbers 0 and 4.");
+
+        int stringArraySelection = Convert.ToInt32(Console.ReadLine());
+
+        if (stringArraySelection > 4 || stringArraySelection < 0)
+        {
+            Console.WriteLine("Sorry this index doesn't exist, please pick a number between 0 and 4.");
+        }
+        else
+        {
+            Console.WriteLine(stringArray[stringArraySelection]);
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Please select an index location of my Integer Array between numbers 0 and 4.");
+        int intArraySelection = Convert.ToInt32(Console.ReadLine());
+
+        if (intArraySelection > 4 || intArraySelection < 0)
+        {
+            Console.WriteLine("Sorry this index doesn't exist, please pick a number between 0 and 4.");
+        }
+        else
+        {
+            Console.WriteLine(intArray[intArraySelection]);
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Please select an index location of my String List between numbers 0 and 4.");
+        int stringListSelection = Convert.ToInt32(Console.ReadLine());
+
+        if (stringListSelection > 4 || stringListSelection < 0)
+        {
+            Console.WriteLine("Sorry this index doesn't exist, please pick a number between 0 and 4.");
+        }
+        else
+        {
+            Console.WriteLine(stringList1[stringListSelection]);
+        }
+
+
+        Console.ReadLine();
+    }
+}
---
+++ Basic_C#_Programs/ArraysAndListsExamplesVideos/ArraysAndListsExamplesVideos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Program
+    {
+        static void Main()
+        {
+
+        List<string> intList = new List<string>();
+        intList.Add("Hello");
+        intList.Add("World!");
+
+        //Console.WriteLine(intList[1]);
+        intList.ForEach(Console.WriteLine); //Writes everything in the list with variable "intList"
+        Console.ReadLine();
+
+        //byte[] byteArray = new byte[5000];
+
+        //List<int> intList = new List<int>();
+        //intList.Add(4);
+        //intList.Add(10);
+        //intList.Remove(10);
+
+        //Console.WriteLine(intList[0]);
+        //Console.ReadLine();
+
+
+
+
+        //int[] numArray = new int[5]; //This says integer array CALLED numArray.
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 }; //this is the exact same as what's above just cleaner and easier to write.
+
+        //int[] numArray2 = { 5, 2, 10, 200, 5000 };
+
+        //numArray2[4] = 5001; 
+
+        ////Console.WriteLine(numArray[3]);
+        ////Console.WriteLine(numArray1[3]);
+        //Console.WriteLine(numArray2[4]);
+        //Console.ReadLine();
+    }
+    }
+

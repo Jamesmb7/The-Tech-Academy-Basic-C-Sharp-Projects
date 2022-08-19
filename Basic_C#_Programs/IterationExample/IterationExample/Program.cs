aa3fad805ff2988cd8265077de0d5a0948df4676
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-18 23:24:40 -0500

    Iteration Uploads

---
+++ Basic_C#_Programs/IterationExample/IterationExample/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+        //for (int i = 0; i < testScores.Length; i++) 
+        //{
+        //    if (testScores[i] > 85)
+        //    {
+        //        Console.WriteLine("Passing test score: " + testScores[i]);
+        //    }
+        //}
+        //Console.ReadLine();
+
+
+        //string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
+
+        //for (int i = 0; i < names.Length; i++)
+        //{
+        //    if (names[i] == "Jesse")
+        //    {
+        //        Console.WriteLine(names[i]);
+        //    }
+        //}
+        //Console.ReadLine();
+
+
+        //string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
+
+        //for (int i = 0; i < names.Length; i++)
+        //{
+        //    Console.WriteLine(names[i]); // Prints the values of the string array "names" (so jesse, erik, daniel, adam will print)
+        //}
+        //Console.ReadLine();
+
+        //List<int> testScores = new List<int>();
+        //testScores.Add(98);
+        //testScores.Add(99);
+        //testScores.Add(81);
+        //testScores.Add(72);
+        //testScores.Add(70);
+
+        //foreach (int score in testScores)
+        //{
+        //    if (score > 85)
+        //    {
+        //        Console.WriteLine("Passing test score:" + score); //Score here is a variable (like i in previous ones) used for the looping. It represents the item in the list we are at. 
+        //    }
+        //}
+        //Console.ReadLine();
+
+        //List<string> names = new List<string>() { "Jesse", "Erik", "Adam", "Daniel" };
+
+        //foreach (string name in names)
+        //{
+        //    if (name == "Jesse")
+        //    {
+        //        Console.WriteLine(name);
+        //    }
+        //}
+        //Console.ReadLine();
+
+        //List<string> names = new List<string>() { "Jesse", "Erik", "Adam", "Daniel" };
+
+        //foreach (string name in names)
+        //{
+        //    Console.WriteLine(name);
+        //}
+        //Console.ReadLine();
+
+        //List<int> testScores = new List<int>() { 98, 99, 12, 74, 23, 99 };
+        //List<int> passingScores = new List<int>(0);
+
+        //foreach (int score in testScores)
+        //{
+        //    if (score > 85)
+        //        passingScores.Add(score);
+        //}
+
+        //Console.WriteLine(passingScores.Count);
+        //Console.ReadLine();
+
+    }
+}
+

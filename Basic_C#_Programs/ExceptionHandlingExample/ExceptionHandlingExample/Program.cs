1b5c26013b6bc75629f1d9596b5ce218be143c8c
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-23 15:55:00 -0500

    StringsAndIntegersUpload

---
+++ Basic_C#_Programs/ExceptionHandlingExample/ExceptionHandlingExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Pick a number");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number.");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Dividing the two...");
+            int numberThree = numberOne / numberTwo;
+            Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            Console.ReadLine();
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please type whole numbers only. Ex. 1, 2, 3 etc. ");
+            //Console.WriteLine(ex.Message); This would print the exact error message given by the software used. You could also make your own error message print (probably easier for users not in tech field).
+            return; //This is put here to show the use for finally. Without readline in finally this return would not allow readline to be reached. Since finally is there, even with this return ending the program finally is reached and readline works.
+        }
+        catch(DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by 0.");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("ex.Message");
+        }
+        finally
+        {
+            Console.ReadLine(); //Putting this inside of finally during a try/catch method, it guarantees that what is in this block of code will be executed regardless of what's in the above code. 
+        }
+        
+
+
+
+
+
+
+
+        //Started with this code, entered 8 and jessie to get a format error. Then created the above try+catch code.
+        //Console.WriteLine("Pick a number");
+        //int numberOne = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Pick a second number.");
+        //int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine("Dividing the two...");
+        //int numberThree = numberOne / numberTwo;
+        //Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+        //Console.ReadLine();
+    }
+}
---
+++ Basic_C#_Programs/StringsAndIntegersSubmission/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> intList = new List<int> { 4, 8, 16, 32, 64 }; //Creates a list of integers
+        Console.WriteLine("Please pick a number to divide the list by!"); //asks a question
+        
+        try
+        {
+            int userNumber = Convert.ToInt32(Console.ReadLine()); //Takes user input for the above question
+            for (int i = 0; i < intList.Count; i++) //for statement 
+            {
+                Console.WriteLine(intList[i] / userNumber);
+            }
+        }
+        catch(FormatException ex)
+        {
+            Console.WriteLine("Use the right format doofus.");
+        }
+        catch(DivideByZeroException ex)
+        {
+            Console.WriteLine("Can't divide by zero doofus.");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("ex.Message");
+        }
+        finally
+        {
+            Console.WriteLine("Doofus, you have emerged.");
+            Console.ReadLine();
+        }
+    }
+}
+

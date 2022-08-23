18168ecf6b31dafc2b49e6c25eee51325cbb1d1d
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-23 18:11:51 -0500

    Update Methods.cs

--- Basic_C#_Programs/CallingMethodsSubmission/CallingMethodsSubmission/Methods.cs
+++ Basic_C#_Programs/CallingMethodsSubmission/CallingMethodsSubmission/Methods.cs
@@ -15,19 +15,16 @@ namespace CallingMethodsSubmission
             return result;
         }
 
+        public int Subtraction(int y)
+        {
+            int result = y - 2;
+            return result;
+        }
 
-
-
-
-        //public Methods Addition()
-        //{
-        //    List<int> addition = new List<int>() { 5, 10, 15, 20 };
-        //    for(int i = 0; i < addition.Count; i++)
-        //    {
-        //        int Answer = choiceOne + addition[i];
-        //        Console.WriteLine(choiceOne + " plus " + addition[i] + " equals " + Answer);
-        //    }
-
-        //}
+        public int Multiply(int z)
+        {
+            int result = z * 2;
+            return result;
+        }
     }
 }

405699096da28802e893ecf10ce67176ffe0f685
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-23 18:11:57 -0500

    Update Program.cs

--- Basic_C#_Programs/CallingMethodsSubmission/CallingMethodsSubmission/Program.cs
+++ Basic_C#_Programs/CallingMethodsSubmission/CallingMethodsSubmission/Program.cs
@@ -13,10 +13,11 @@ namespace CallingMethodsSubmission
             Console.WriteLine("Pick a number between 1 and 10 to do some math!");
             int choiceOne = Convert.ToInt32(Console.ReadLine());
 
-            Methods obj1 = new Methods();
-            Console.WriteLine(obj1.Addition(choiceOne));
+            Methods obj1 = new Methods(); //creates an object of the Methods class and names the object obj1
+            Console.WriteLine(obj1.Addition(choiceOne)); //Calling addition method through obj1 object and passing in choiceOne
+            Console.WriteLine(obj1.Subtraction(choiceOne));
+            Console.WriteLine(obj1.Multiply(choiceOne));
             Console.ReadLine();
         }
-
     }
 }

6f15c86c473fe5671f1bc8d589331f16654fd8b1
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-23 15:57:39 -0500

    Update Program.cs

--- Basic_C#_Programs/StringsAndIntegersSubmission/ConsoleApp1/Program.cs
+++ Basic_C#_Programs/StringsAndIntegersSubmission/ConsoleApp1/Program.cs
@@ -18,25 +18,25 @@ class Program
             int userNumber = Convert.ToInt32(Console.ReadLine()); //Takes user input for the above question
             for (int i = 0; i < intList.Count; i++) //for statement 
             {
-                Console.WriteLine(intList[i] / userNumber);
+                Console.WriteLine(intList[i] / userNumber); //takes the value at integer i in the intlist and divides that by the user input. 
             }
         }
-        catch(FormatException ex)
+        catch(FormatException ex)//catch statement to display format errors and print it in console. (trying to divide by a string etc.)
         {
-            Console.WriteLine("Use the right format doofus.");
+            Console.WriteLine("Use the right format doofus.");           
         }
-        catch(DivideByZeroException ex)
+        catch(DivideByZeroException ex) //catch statement to display an attempted divide by zero (not possible)
         {
             Console.WriteLine("Can't divide by zero doofus.");
         }
-        catch(Exception ex)
+        catch(Exception ex) //general exception catch incase something was missed above
         {
             Console.WriteLine("ex.Message");
         }
         finally
         {
-            Console.WriteLine("Doofus, you have emerged.");
-            Console.ReadLine();
+            Console.WriteLine("Doofus, you have emerged."); //in finally will show when you have finished this loop (basically)
+            Console.ReadLine();//keeps the console window open long enough to review data
         }
     }
 }

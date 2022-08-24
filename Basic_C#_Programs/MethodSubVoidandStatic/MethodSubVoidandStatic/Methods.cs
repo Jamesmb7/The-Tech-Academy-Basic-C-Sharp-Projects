067ae0232cd3365e5f92d8ff8f096d0256b953c7
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-24 16:47:49 -0500

    void and static updates

--- Basic_C#_Programs/MethodSubVoidandStatic/MethodSubVoidandStatic/Methods.cs
+++ Basic_C#_Programs/MethodSubVoidandStatic/MethodSubVoidandStatic/Methods.cs
@@ -8,29 +8,29 @@ namespace MethodSubVoidandStatic
 {
     class Methods
     {
-        public void divided(int a)
+        public void divided(int a) //creates a method called divided
         {
-            int result = a / 2;
-            Console.WriteLine(a + " divided by 2 = " + result);
+            int result = a / 2; // math equation to take user in put and divide by 2
+            Console.WriteLine(a + " divided by 2 = " + result); //writes to console 
         }
 
-        public void divided(int e, int f)
+        public void divided(int e, int f) //overload the method called divided, adding an additional integer to allow overloading.
         {
-            int result = (e + f) / 2;
-            Console.WriteLine(e + " " + f + " divided by 2 = " + result);
+            int result = (e + f) / 2; //math equation to take user input and divide by 2
+            Console.WriteLine(e + " " + f + " divided by 2 = " + result); //writes to console
         }
 
-        public int addition1(int a, out int b)
+        public int addition1(int a, out int b) //uses out in the method for using an output parameter (requires at least 1 input, int a for example)
         {
-            b = 2;
-            int d = a + b;
-            return d;
+            b = 2; //assigns a value to b
+            int d = a + b; //does a math equation with int a and b and assigns that value to d
+            return d; //returns the value of d.
         }
 
-        public static int something(int g, int h)
+        public static int something(int g, int h) //creates a static method (think of a car, it has 4 wheels, that never changes. 3 wheels woulnd't be a car, 2 wheels wouldn't be a car etc.)
         {
-            int y = g * h;
-            return y;
+            int y = g * h; // math equation using the assigned variables. 
+            return y; //returns the value of the math equation
         }
     }   
 }
--- Basic_C#_Programs/MethodSubVoidandStatic/MethodSubVoidandStatic/Program.cs
+++ Basic_C#_Programs/MethodSubVoidandStatic/MethodSubVoidandStatic/Program.cs
@@ -10,14 +10,14 @@ namespace MethodSubVoidandStatic
     {
         static void Main(string[] args)
         {
-            Methods obj1 = new Methods();
-            Console.WriteLine("Please enter a number");
-            int a = Convert.ToInt32(Console.ReadLine());
-            obj1.divided(a);
+            Methods obj1 = new Methods(); //instantiates the class and creates an object
+            Console.WriteLine("Please enter a number"); //asks a question
+            int a = Convert.ToInt32(Console.ReadLine()); //assigns user input valute to a, converted into an integer
+            obj1.divided(a); // calls the method on the value a
 
 
-            int z = Methods.something(5, 6);
-            Console.WriteLine(z);
+            int z = Methods.something(5, 6); //calls the method something and adds values to it.
+            Console.WriteLine(z); //displays the result of method something with the values above, using int z
 
             Console.ReadLine();
         }

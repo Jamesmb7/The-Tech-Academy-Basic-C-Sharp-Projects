e82083f54485e45d9c3a74475247167615a5f66b
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-23 22:41:48 -0500

    MethodSubmissionAssignmentUploads

---
+++ Basic_C#_Programs/MethodSubmissionWithTwoIntegers/MethodSubmissionWithTwoIntegers/Methods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionWithTwoIntegers
+{
+    class Methods
+    {
+        public int twoIntegers(int a, [Optional] int b)
+        {
+            int result = a + b;
+            return result;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodSubmissionWithTwoIntegers/MethodSubmissionWithTwoIntegers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionWithTwoIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods obj1 = new Methods();
+            int b;
+            int result;
+            Console.WriteLine("Please give me a number between 1 and 9.");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Thank you. Pick another number between 1 and 9. --optional");
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            if (int.TryParse(Console.ReadLine(), out b))
+                result = obj1.twoIntegers(b);
+
+            
+            Console.WriteLine(obj1.twoIntegers(a, b));
+            Console.ReadLine();
+        }
+    }
+}

15c208f94f7d9903f95882ce04aeee22040552ee
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-23 21:23:17 -0500

    Method Uploads and Updates

---
+++ Basic_C#_Programs/MainMethodSubmission/MainMethodSubmission/Methods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class Methods
+    {
+        public int Addition(int x) //creates a method in the Methods class
+        {
+            int result = x + 5; //creates an integer math equation
+            return result; //returns the results of this equation
+        }
+        public int Subtraction(decimal y) //creates a method in the Methods class
+        {
+            int result =Convert.ToInt32(y - 5.5m); //converts a decimal to an integer and does a math equation math equation
+            return result; //returns the results of this equation
+        }
+        public int multiply(string z) //creates a method in the Methods class
+        {
+            int result =Convert.ToInt32(z) * 5; //converts the string to an integer and does a math equation
+            return result; //returns the results of this equation
+        }
+    }
+}
---
+++ Basic_C#_Programs/MainMethodSubmission/MainMethodSubmission/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)            
+        { 
+            int x = 5;
+            decimal y = 35m;
+            string z = "5";
+            Methods obj1 = new Methods();
+
+            Console.WriteLine(obj1.Addition(x));
+            Console.WriteLine(obj1.Subtraction(y));
+            Console.WriteLine("I need a number for a string-to-int conversion! What's a cool number?");
+            Console.WriteLine(obj1.multiply(z));
+
+
+            Console.ReadLine();
+        }
+    }
+}

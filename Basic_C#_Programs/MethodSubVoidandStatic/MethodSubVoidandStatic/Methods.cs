df3dcbea41bf74caddcd05ef6b46b54e8473b7c6
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-24 16:42:07 -0500

    VoidandStatic Upload

---
+++ Basic_C#_Programs/MethodSubVoidandStatic/MethodSubVoidandStatic/Methods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubVoidandStatic
+{
+    class Methods
+    {
+        public void divided(int a)
+        {
+            int result = a / 2;
+            Console.WriteLine(a + " divided by 2 = " + result);
+        }
+
+        public void divided(int e, int f)
+        {
+            int result = (e + f) / 2;
+            Console.WriteLine(e + " " + f + " divided by 2 = " + result);
+        }
+
+        public int addition1(int a, out int b)
+        {
+            b = 2;
+            int d = a + b;
+            return d;
+        }
+
+        public static int something(int g, int h)
+        {
+            int y = g * h;
+            return y;
+        }
+    }   
+}
---
+++ Basic_C#_Programs/MethodSubVoidandStatic/MethodSubVoidandStatic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubVoidandStatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods obj1 = new Methods();
+            Console.WriteLine("Please enter a number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            obj1.divided(a);
+
+
+            int z = Methods.something(5, 6);
+
+            Console.ReadLine();
+        }
+    }
+}

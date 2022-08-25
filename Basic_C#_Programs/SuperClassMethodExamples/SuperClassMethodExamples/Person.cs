5ee13c585ba829779f34dcf368ff91c5a99faffc
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-24 20:05:05 -0500

    Update Person.cs

--- Basic_C#_Programs/SuperClassMethodExamples/SuperClassMethodExamples/Person.cs
+++ Basic_C#_Programs/SuperClassMethodExamples/SuperClassMethodExamples/Person.cs
@@ -8,12 +8,12 @@ namespace SuperClassMethodExamples
 {
     class Person
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FirstName { get; set; } //Sets a property called FirstName
+        public string LastName { get; set; } //Sets a property called LastName
 
-        public void SayName()
+        public void SayName() //Creates a method
         {
-            Console.WriteLine("Name: " + FirstName + " " + LastName);
+            Console.WriteLine("Name: " + FirstName + " " + LastName); //Writes to console.
         }
     }
 }

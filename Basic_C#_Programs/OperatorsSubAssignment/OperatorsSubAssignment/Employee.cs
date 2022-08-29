3897be2e0562ee16d3f4a199a64b2e10c202f02b
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-29 14:57:09 -0500

    Updates

--- Basic_C#_Programs/OperatorsSubAssignment/OperatorsSubAssignment/Employee.cs
+++ Basic_C#_Programs/OperatorsSubAssignment/OperatorsSubAssignment/Employee.cs
@@ -8,18 +8,18 @@ namespace OperatorsSubAssignment
 {
     public class Employee
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public int Id { get; set; } //creates a property
+        public string FirstName { get; set; } //creates a property
+        public string LastName { get; set; } //creates a property
 
-        public static bool operator == (Employee One, Employee Two  )
+        public static bool operator == (Employee One, Employee Two  ) //overloads the == operator so it checks 2 employee's id
         {
-            return One.Id == Two.Id; 
+            return One.Id == Two.Id;  //compares the id values of employee One and employee Two
         }
 
-        public static bool operator != (Employee One, Employee Two )
+        public static bool operator != (Employee One, Employee Two ) //overloads the != operator to check 2 employee's id, have to have comparison operators in pairs. 
         {
-            return One.Id != Two.Id;
+            return One.Id != Two.Id; //compares the id values of employee One and employee Two
         }
     }
 }
--- Basic_C#_Programs/OperatorsSubAssignment/OperatorsSubAssignment/Program.cs
+++ Basic_C#_Programs/OperatorsSubAssignment/OperatorsSubAssignment/Program.cs
@@ -10,14 +10,14 @@ namespace OperatorsSubAssignment
     {
         static void Main(string[] args)
         {
-            Employee employee1 = new Employee();
-            Employee employee2 = new Employee();
+            Employee employee1 = new Employee(); //instantiates an employee
+            Employee employee2 = new Employee(); //instantiates a 2nd employee
 
-            employee1.Id = 5;
-            employee2.Id = 5;
+            employee1.Id = 5; //gives a value to employee1 ID
+            employee2.Id = 5; //gives a value to employee2 ID
 
-            Console.WriteLine(employee1 == employee2);
-            Console.ReadLine();
+            Console.WriteLine(employee1 == employee2); //writes the comparison to console to be a true or false bool arguement. 
+            Console.ReadLine(); //lets you view the data without console closing.
         }
     }
 }

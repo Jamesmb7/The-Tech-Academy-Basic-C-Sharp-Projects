38c4fb9016a2980e9b3fd77ec03e73b0423efca5
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-29 15:43:06 -0500

    Updates

--- Basic_C#_Programs/ParameterAssignusingGenerics/ParameterAssignusingGenerics/Program.cs
+++ Basic_C#_Programs/ParameterAssignusingGenerics/ParameterAssignusingGenerics/Program.cs
@@ -10,6 +10,21 @@ namespace ParameterAssignusingGenerics
     {
         static void Main(string[] args)
         {
+            Employee<string> things = new Employee<string>();
+            Employee<int> things2 = new Employee<int>();
+
+            things.Things = new List<string> { "James", "Jackie", "Jackson" };
+            things2.Things = new List<int> { 1, 2, 3, };
+
+            for (int i = 0; i < things.Things.Count; i++)
+            {
+                Console.WriteLine(things.Things[i]);
+            }
+            for (int i = 0; i < things2.Things.Count; i++)
+            {
+                Console.WriteLine(things2.Things[i]);
+            }
+            Console.ReadLine();
         }
     }
 }

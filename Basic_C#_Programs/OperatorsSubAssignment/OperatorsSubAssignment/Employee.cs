67dabf049f980d2029667b90c7e84e442f7eaf05
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-29 14:30:10 -0500

    Operator Updates

---
+++ Basic_C#_Programs/OperatorsSubAssignment/OperatorsSubAssignment/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubAssignment
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator== (Employee id, Employee FirstName  )
+        {
+            
+        }
+
+        public static bool operator != (Employee id, Employee FirstName )
+        {
+
+        }
+    }
+}

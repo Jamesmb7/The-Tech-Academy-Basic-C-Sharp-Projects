95699ffd8feb456aec3ca346b1d5e8d7d8a2d2c4
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-30 14:18:30 -0500

    Lambda Updates

--- Basic_C#_Programs/LambdaSubAssign/LambdaSubAssign/Program.cs
+++ Basic_C#_Programs/LambdaSubAssign/LambdaSubAssign/Program.cs
@@ -10,28 +10,65 @@ namespace LambdaSubAssign
     {
         static void Main(string[] args)
         {
-            List<string> employeesfName = new List<string>() { "Joe", "James", "John", "Dalton", "Levi", "Andrew", "Joe", "Tom", "Darrel", "Neil" };
-            List<string> employeeJoe = new List<string>();
-            string hello = "Joe";
 
-            Employee fname = new Employee();
-            Employee lname = new Employee();
-            Employee id = new Employee();
-
-
-            foreach (string name in employeesfName)
+            List<Employee> employees = new List<Employee>() //Employee employee = new Employee (but as a list) using the Employee class as a data type
             {
-                if (employeesfName.Contains("Joe"))
+                new Employee(){Id = 0, FirstName = "Joe", LastName = "Swanson"},
+                new Employee(){Id = 1, FirstName = "Jack", LastName = "Hanes"},
+                new Employee(){Id = 2, FirstName = "Aaron", LastName = "Rodgers"},
+                new Employee(){Id = 3, FirstName = "Mason", LastName = "Kase"},
+                new Employee(){Id = 4, FirstName = "Neil", LastName = "Jordan"},
+                new Employee(){Id = 5, FirstName = "Blake", LastName = "Griffin"},
+                new Employee(){Id = 6, FirstName = "Joe", LastName = "Jackson"},
+                new Employee(){Id = 7, FirstName = "James", LastName = "Bennett"},
+                new Employee(){Id = 8, FirstName = "Frank", LastName = "Helps"},
+                new Employee(){Id = 9, FirstName = "Dalton", LastName = "Smith"},
+            };
+            List<Employee> joeList = new List<Employee>();
+
+            foreach (Employee employee in employees)
+            {
+                if (employee.FirstName == "Joe")
                 {
-                    employeeJoe.Add("Joe");
+                    joeList.Add(employee);
                 }
-                
             }
-            Console.WriteLine(employeeJoe);
+            foreach (Employee employee in joeList)
+            {
+                Console.WriteLine(employee.FirstName + " " + employee.LastName + " " + employee.Id);
+            }
+
+            Console.WriteLine();
+
+            List<Employee> joeList2 = employees.FindAll(x => x.FirstName == "Joe").ToList();
+            foreach (Employee employee in joeList2)
+            {
+                Console.WriteLine(employee.FirstName + " " + employee.LastName + " " + employee.Id);
+            }
+
+            Console.WriteLine();
+
+            List<Employee> idList = employees.Where(x => x.Id > 5).ToList();
+            foreach (Employee employee in idList)
+            {
+                Console.WriteLine(employee.FirstName + " " + employee.LastName + " " + employee.Id);
+            }
             Console.ReadLine();
 
-            //List<string> newlist = fname.FirstName.Where(x => x == "Joe").ToList();
-            //int newID = id.Where(x => x > 5).Sum();
+
+
+            //foreach (Employee name in employees) This is wrong**
+            //{
+            //    if (employees.Contains("Joe"))
+            //    {
+            //        employeeJoe.Add("Joe");
+            //    }
+
+            //}
+            //Console.WriteLine(employeeJoe);
+            //Console.ReadLine();
+
+
 
         }
     }

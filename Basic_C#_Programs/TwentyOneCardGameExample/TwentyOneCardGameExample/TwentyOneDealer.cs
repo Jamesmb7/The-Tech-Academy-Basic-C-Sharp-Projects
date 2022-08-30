e65ef208b6e2902434c9fee7c53033c36628f1ff
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-08-30 16:34:29 -0500

    Updates

--- Basic_C#_Programs/TwentyOneCardGameExample/TwentyOneCardGameExample/TwentyOneDealer.cs
+++ Basic_C#_Programs/TwentyOneCardGameExample/TwentyOneCardGameExample/TwentyOneDealer.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace TwentyOneCardGameExample
 {
-    class TwentyOneDealer
+    public class TwentyOneDealer : Dealer
     {
+        public List<Card> Hand { get; set; }
+        public bool Stay { get; set; }
+        public bool isBusted { get; set; }
     }
 }

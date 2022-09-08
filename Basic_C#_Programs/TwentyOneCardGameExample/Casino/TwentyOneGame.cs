0052c184028cad02bc3a78a76b9f8d74fa8350c2
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-09-07 20:01:19 -0500

    Updates 21 card game

--- Basic_C#_Programs/TwentyOneCardGameExample/Casino/TwentyOneGame.cs
+++ Basic_C#_Programs/TwentyOneCardGameExample/Casino/TwentyOneGame.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Casino.Interfaces;
 
-namespace Casino.TwentyOneCardGameExample
+namespace Casino.TwentyOne
 {
     public class TwentyOneGame : Game, IWalkAway
     {
--- Basic_C#_Programs/TwentyOneCardGameExample/TwentyOneCardGameExample/Program.cs
+++ Basic_C#_Programs/TwentyOneCardGameExample/TwentyOneCardGameExample/Program.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Casino;
-using Casino.TwentyOneCardGameExample;
+using Casino.TwentyOne;
 
-namespace TwentyOneCardGameExample
+namespace TwentyOne
 {
     class Program
     {

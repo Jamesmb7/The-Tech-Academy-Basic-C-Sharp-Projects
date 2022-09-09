8b4168f66fb1967b4763bf46c41dc9742ddc867d
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-09-09 14:06:45 -0500

    Constructor Assignment Sub

---
+++ Basic_C#_Programs/ConstructorSubAssignment/ConstructorSubAssignment/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubAssignment
+{
+    public class Employee
+    {
+        public int id; //properties
+        public string name; //properties
+
+        public Employee(int id, string name)//main constructor
+        {
+            this.id = id; //Assigns value to id.
+            this.name = name; //Assigns value to name.
+        }
+
+        public Employee() : this(12, "Joe")//chain
+        {
+        }
+
+        public Employee(int id) : this(id, "Howdy")//chain
+        {
+        }
+
+        public Employee(string name) :this(0, name)//chain
+        {
+        }
+    }
+}
--- Basic_C#_Programs/ConstructorSubAssignment/ConstructorSubAssignment/Program.cs
+++ Basic_C#_Programs/ConstructorSubAssignment/ConstructorSubAssignment/Program.cs
@@ -13,6 +13,16 @@ namespace ConstructorSubAssignment
             const string myName = "James";
             var fastFood = "Sonic";
 
+            Employee employee1 = new Employee(); //instantiating the constructors
+            Console.WriteLine(employee1.name + " " + employee1.id);
+            Employee employee2 = new Employee(5, "Hello");
+            Console.WriteLine(employee2.name + " " + employee2.id);
+            Employee employee3 = new Employee(7);
+            Console.WriteLine(employee3.name + " " + employee3.id);
+            Employee employee4 = new Employee("Jack");
+            Console.WriteLine(employee4.name + " " + employee4.id);
+            Console.ReadLine();
+
 
         }
     }

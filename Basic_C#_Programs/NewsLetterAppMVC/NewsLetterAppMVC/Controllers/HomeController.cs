68c83c6a74b6ba84608f46f7513b321102724858
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-10-14 16:44:13 -0500

    updates

--- Basic_C#_Programs/NewsLetterAppMVC/NewsLetterAppMVC/Controllers/HomeController.cs
+++ Basic_C#_Programs/NewsLetterAppMVC/NewsLetterAppMVC/Controllers/HomeController.cs
@@ -13,6 +13,19 @@ namespace NewsLetterAppMVC.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                return View("Success");
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";

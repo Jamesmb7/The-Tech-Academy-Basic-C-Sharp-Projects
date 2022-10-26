92bce6aed45fc897054504006d1c86a1b5189a5b
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-10-25 23:01:46 -0500

    Updates

--- Basic_C#_Programs/NewsLetterAppMVC/NewsLetterAppMVC/Newsletter.Designer.cs
+++ Basic_C#_Programs/NewsLetterAppMVC/NewsLetterAppMVC/Newsletter.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\Tech_Academy\The-Tech-Academy-Basic-C-Sharp-Projects\Basic_C#_Programs\NewsLetterAppMVC\NewsLetterAppMVC\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\Tech_Academy\The-Tech-Academy-Basic-C-Sharp-Projects\Basic_C#_Programs\NewsLetterAppMVC\NewsLetterAppMVC\Newsletter.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.

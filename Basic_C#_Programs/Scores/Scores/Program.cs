cc1407bfbe22f393697464f630376c4b7b177e81
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-11-08 19:17:11 -0600

    Progress

---
+++ Basic_C#_Programs/Scores/Scores/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter in your first name.");
+            string date = DateTime.Today.ToShortDateString();
+            string uName = Console.ReadLine();
+            string msg = $"\nWelcome back {uName}. Today is {date}."; //reminder \n puts on a new line. {uName} etc calls the variable/string and it's data the easy way. 
+            Console.WriteLine(msg);
+
+            string path = @"E:\Tech_Academy\The-Tech-Academy-Basic-C-Sharp-Projects\Basic_C#_Programs\Scores\Scores\studentScores.txt";
+            string[] lines = System.IO.File.ReadAllLines(path); //Grabes the files from the PATH (hence the reason path is here) and takes the data from said file puts it into array.
+            double tScore = 0.0;
+
+            Console.WriteLine("\nStudent Scores: \n");
+            foreach(string line in lines) {
+                Console.WriteLine("\n" + line);
+                double score = Convert.ToDouble(line);
+                tScore += score;
+            }
+
+            double avgScore = tScore / lines.Length;
+            Console.WriteLine("\nTotal of " + lines.Length + " student scores. \tAverage score: " + avgScore);
+
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/helloworldCore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace helloworldCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey(); //does the same as writeline, not sure why this readkey was used here in the example. 
+        }
+    }
+}

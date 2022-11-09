32dbcbdc321b18c94bad02bd7d2e7e7002458796
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-11-08 22:59:20 -0600

    StartingWorkFinalProject

---
+++ Basic_C#_Programs/FinalChallengeAssignment/FinalChallengeAssignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace FinalChallengeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+    
+    public class Grade
+    {
+        public int GradeID { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+}
+}
